e10ec29545446156f79027b5c3fe39af1ce09a21
ZOUQUAN-A <89234036+ZQDesigned@users.noreply.github.com>
2022-05-20 00:04:57 +0800

    新增接口集合

--- app/src/main/java/UTSSG/ZQDesigned/Characterscode/MigrateActivity.java
+++ app/src/main/java/UTSSG/ZQDesigned/Characterscode/MigrateActivity.java
@@ -8,10 +8,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.UnsupportedEncodingException;
+
+import UTSSG.ZQDesigned.Characterscode.Utils.FunctionsToolset;
 
 public class MigrateActivity extends AppCompatActivity {
 
@@ -32,16 +33,12 @@ public class MigrateActivity extends AppCompatActivity {
 
         if(TextUtils.isEmpty(chractersStr)) {//判断
             Toast.makeText(this, "请输入内容！", Toast.LENGTH_SHORT).show();
-            new AlertDialog.Builder(this).setTitle("提示")//设置对话框标题
-                    .setMessage("请输入内容！")//设置显示的内容
-                    .setNegativeButton("返回", null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)//设置图标
-                    .setCancelable(false)//设置点击对话框外部不消失
-                    .show();//在按键响应事件中显示此对话框
+            FunctionsToolset.showDialog(this, "提示","请输入内容！","返回",android.R.drawable.ic_dialog_alert,false);
             return;
         }
 
         TextView result = findViewById(R.id.output);
+        EditText result_long = findViewById(R.id.output_long);
         byte[] codeBit = chractersStr.getBytes("gb2312");// 获取汉字的字节数组
         if(codeBit.length%2==0) {//判断
             StringBuilder sb = new StringBuilder();
@@ -50,13 +47,7 @@ public class MigrateActivity extends AppCompatActivity {
 
                 if (codeBit_tmp < 0) {
                     Toast.makeText(this, "您输入的内容不合法！", Toast.LENGTH_SHORT).show();
-                    new AlertDialog.Builder(this).setTitle("提示")//设置对话框标题
-                            .setMessage("您输入的内容不合法！")//设置显示的内容
-                            .setNegativeButton("返回", (dialog, which) -> {
-                            })
-                            .setIcon(android.R.drawable.ic_dialog_alert)//设置对话框图标
-                            .setCancelable(false)//设置点击对话框外部不消失
-                            .show();//在按键响应事件中显示此对话框
+                    FunctionsToolset.showDialog(this,"提示","您输入的内容不合法！","返回",android.R.drawable.ic_dialog_alert,false);
                     return;
                 } else if (codeBit_tmp <= 10) {
                     sb.append(0);
@@ -65,14 +56,10 @@ public class MigrateActivity extends AppCompatActivity {
             }
             Toast.makeText(this, "正在查询......", Toast.LENGTH_SHORT).show();
             result.setText(sb.toString().replaceAll(".{4}", "$0 "));
+            result_long.setText(sb.toString().replaceAll(".{4}", "$0 "));
         }else{
             Toast.makeText(this, "您输入的内容不合法！", Toast.LENGTH_SHORT).show();
-            new AlertDialog.Builder(this).setTitle("提示")//设置对话框标题
-                    .setMessage("您输入的内容不合法！")//设置显示的内容
-                    .setNegativeButton("返回", null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)//设置对话框图标
-                    .setCancelable(false)//设置点击对话框外部不消失
-                    .show();//在按键响应事件中显示此对话框
+            FunctionsToolset.showDialog(this,"提示","您输入的内容不合法！","返回",android.R.drawable.ic_dialog_alert,false);
         }
 
     }
--- app/src/main/java/UTSSG/ZQDesigned/Characterscode/Utils/FunctionsToolset.java
+++ app/src/main/java/UTSSG/ZQDesigned/Characterscode/Utils/FunctionsToolset.java
@@ -1,10 +1,12 @@
 package UTSSG.ZQDesigned.Characterscode.Utils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 
-public class FunctionsToolset {
+import UTSSG.ZQDesigned.Characterscode.R;
 
+public class FunctionsToolset {
 
 
     /**
@@ -13,7 +15,7 @@ public class FunctionsToolset {
      * @param permissionName
      * @param requestCode
      */
-    public static void requestPermission(Activity activity,String permissionName, int requestCode) {
+    public static void requestPermission(Activity activity, String permissionName, int requestCode) {
         if (activity.checkSelfPermission(permissionName) != PackageManager.PERMISSION_GRANTED) {
             activity.requestPermissions(new String[]{permissionName}, requestCode);
         }
@@ -24,7 +26,7 @@ public class FunctionsToolset {
      *
      * @param permissionName
      * @param activity
-     * @return
+     * @return true 有权限，false 没有权限
      */
     public static boolean checkPermission(Activity activity, String permissionName) {
         int check = activity.getApplicationContext().checkCallingOrSelfPermission(permissionName);
@@ -37,10 +39,10 @@ public class FunctionsToolset {
      * @param packageName
      * @return
      */
-    public static boolean isInstalled(Activity activity,String packageName) {
+    public static boolean isInstalled(Activity activity, String packageName) {
         FunctionsToolset functionsToolset = new FunctionsToolset();
         try {
-            functionsToolset.pm(activity,packageName);
+            functionsToolset.pm(activity, packageName);
             return true;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
@@ -48,7 +50,7 @@ public class FunctionsToolset {
 
     }
 
-    void pm(Activity activity,String packageName) throws PackageManager.NameNotFoundException {
+    void pm(Activity activity, String packageName) throws PackageManager.NameNotFoundException {
         activity.getPackageManager().getPackageInfo(packageName, 0);
     }
 
@@ -58,8 +60,28 @@ public class FunctionsToolset {
      * @param activity
      * @param color
      */
-    public static void setStatusBarColor(Activity activity,int color) {
+    public static void setStatusBarColor(Activity activity, int color) {
         activity.getWindow().setStatusBarColor(color);
+    }
+
+    /**
+     * 弹出对话框
+     *
+     * @param activity
+     * @param title
+     * @param message
+     * @param positiveButton
+     * @param icon
+     * @param cancelable
+     */
+    public static void showDialog(Activity activity, String title, String message, String positiveButton,int icon,boolean cancelable) {
+        new AlertDialog.Builder(activity)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positiveButton, null)
+                .setIcon(icon)
+                .setCancelable(cancelable)
+                .show();
     }
 }
 
